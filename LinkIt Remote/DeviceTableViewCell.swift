a6cffe9c460836a25d0389f2a15aeb62ec1e2124
Pablo Sun <pablo.sun@mediatek.com>
2017-08-23 11:58:39 +0800

    Add dummy device cells

---
+++ LinkIt Remote/DeviceTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  DeviceTableViewCell.swift
+//  LinkIt Remote
+//
+//  Created by Pablo Sun on 2017/8/23.
+//  Copyright © 2017年 MediaTek Labs. All rights reserved.
+//
+
+import UIKit
+
+class DeviceTableViewCell: UITableViewCell {
+    //MARK: properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    
+    //MARK: cell methods
+    override func awakeFromNib() {
+                super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- LinkIt Remote/DeviceTableViewController.swift
+++ LinkIt Remote/DeviceTableViewController.swift
@@ -11,18 +11,58 @@ import UIKit
 class DeviceTableViewController: UITableViewController {
     //MARK: properties
     var devices : [Device] = [Device]()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("device count = \(devices.count)")
+        loadSampleDevices()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return devices.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellId = "DeviceTableViewCell"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as? DeviceTableViewCell else {
+            fatalError("wrong cached cell type")
+        }
+        
+        // Configure the cell...
+        let device = devices[indexPath.row]
+        cell.nameLabel.text = device.name
+        cell.addressLabel.text = device.address
+        
+        // return it to framework for caching/drawing
+        return cell
+    }
 
+
+
+    //MARK: private methods
+    private func loadSampleDevices() {
+        
+        for index in 0..<3 {
+            let device = Device(name: "Test \(index)", address: "--:--:--:--")
+            print("device: \(device)")
+            devices.append(device)
+        }
+    }
 
 }
 
