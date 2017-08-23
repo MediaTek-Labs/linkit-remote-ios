382e02f790d77122e4ab33b0e287145e62b441bc
Pablo Sun <pablo.sun@mediatek.com>
2017-08-23 09:49:31 +0800

    Initial Commit

---
+++ LinkIt Remote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LinkIt Remote
+//
+//  Created by Pablo Sun on 2017/8/23.
+//  Copyright © 2017年 MediaTek Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
